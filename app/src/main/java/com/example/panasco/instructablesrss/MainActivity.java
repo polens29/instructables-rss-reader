bd9034b9f71259fa726c38b59c9f20901c0ffe7b
Pauline Anasco <polens29@gmail.com>
2017-01-18 00:06:39 +0800

    change cursor

--- app/src/main/java/com/example/panasco/instructablesrss/MainActivity.java
+++ app/src/main/java/com/example/panasco/instructablesrss/MainActivity.java
@@ -2,15 +2,22 @@ package com.example.panasco.instructablesrss;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.LinearLayout;
 
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.lang.reflect.Field;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,6 +43,30 @@ public class MainActivity extends AppCompatActivity {
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+        MenuItem item = menu.findItem(R.id.search_menu);
+
+        SearchView sv = (SearchView)item.getActionView();
+        sv.setQueryHint(getResources().getString(R.string.search_hint));
+        sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                setContentView(R.layout.search_results);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
     }
 
     public void open_chrome(View view) {
