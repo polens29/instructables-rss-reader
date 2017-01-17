039ba8b64f3630a598282a10a8af673320279f78
Pauline Anasco <polens29@gmail.com>
2017-01-17 22:48:21 +0800

    added search

--- app/src/main/java/com/example/panasco/instructablesrss/MainActivity.java
+++ app/src/main/java/com/example/panasco/instructablesrss/MainActivity.java
@@ -1,13 +1,65 @@
 package com.example.panasco.instructablesrss;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.LinearLayout;
+
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.activity_main);
+
+
+        /*LinearLayout linear_layout = (LinearLayout) findViewById(R.id.activity_main);
+
+        TextView headline = new TextView(this);
+        headline.setText("Hello Worlds!!!");
+        headline.setTextSize(50);
+        linear_layout.addView(headline);*/
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+    }
+
+    public void open_chrome(View view) {
+        String url = "http://www.facebook.com";
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        i.setPackage("com.android.chrome");
+        try {
+            startActivity(i);
+        } catch (ActivityNotFoundException e) {
+            // Chrome is probably not installed
+            // Try with the default browser
+            i.setPackage(null);
+            startActivity(i);
+        }
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        client.disconnect();
     }
 }
